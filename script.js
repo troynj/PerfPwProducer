0ead25fe151d21f806b6e76c8b1c4fb8b3e8793c
Troy Johnson <troynjohnson6@gmail.com>
2022-12-22 16:15:43 -0800

    adding logic for setting desired password options

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var desiredChar = [];
+var desiredLength;
+
 
 // Write password to the #password input
 function writePassword() {
@@ -7,7 +10,45 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function setPwParams () {
+    var charOptions = ['lowercase', 'uppercase', 'numeric', 'special']
+    
+
+    setPwLength();
+
+    for (var i = 0; i < charOptions.length; i++) {
+      if (setPwChar(charOptions[i])) {
+    desiredChar.push(charOptions[i])
+    console.log("char = " + desiredChar)
+      }
+    }
+}
+
+function setPwLength () {
+    var tempDesiredLength;
+    var lengthCheck = false;
+    
+    while (!lengthCheck) {
+    tempDesiredLength = prompt("How long would you like your password to be? Enter a number between 8 and 128 (inclusive)");
+
+    if(tempDesiredLength >= 8 && tempDesiredLength <= 128){
+        lengthCheck = true;
+        desiredLength = tempDesiredLength;
+    }
+    else {
+        alert("Please enter a number between 8 and 128 (inclusive)");
+    }
+
+    }
+
+    return;
+
+}
+
+function setPwChar(type) {
+return confirm("Would you like the password to contain " + type + " characters?")
 }
 
 // Add event listener to generate button
